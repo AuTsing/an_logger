46785d1a840b48794e35137d363e299ca604a90a
AuTsing <763025696@qq.com>
2023-05-23 16:11:59 +0800

    remove: jni

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
 use android_logger::Config;
-use jni::errors::Error;
-use jni::objects::GlobalRef;
-use jni::objects::JMethodID;
-use jni::objects::JValue;
-use jni::signature::Primitive;
-use jni::signature::ReturnType;
-use jni::Executor;
-use jni::JNIEnv;
 use log::Level;
 use once_cell::sync::Lazy;
 use std::ffi::CStr;
@@ -16,7 +8,6 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::os::unix::prelude::FromRawFd;
 use std::os::unix::prelude::RawFd;
-use std::sync::Arc;
 use std::thread;
 
 static TAG: Lazy<&CStr> = Lazy::new(|| CStr::from_bytes_with_nul(b"AnLogger\0").unwrap());
@@ -38,36 +29,13 @@ fn android_log(level: Level, tag: &CStr, msg: &CStr) {
 #[cfg(not(target_os = "android"))]
 fn android_log(_level: Level, _tag: &CStr, _msg: &CStr) {}
 
-fn try_log_write(buffer: String) {
-    if let Ok(msg) = CString::new(buffer) {
+fn try_log_write(buffer: &String) {
+    if let Ok(msg) = CString::new(buffer.clone()) {
         android_log(Level::Info, &TAG, &msg);
     }
 }
 
-fn try_log_app(
-    exec: &Executor,
-    io_globalref: &GlobalRef,
-    io_log_info_jmethodid: &JMethodID,
-    buffer: String,
-) {
-    exec.with_attached_capacity(4, |env| {
-        let msg = buffer.trim_end();
-        let msg_jstring = env.new_string(msg)?;
-        unsafe {
-            env.call_method_unchecked(
-                io_globalref,
-                io_log_info_jmethodid,
-                ReturnType::Primitive(Primitive::Void),
-                &[JValue::from(&msg_jstring).as_jni()],
-            )?;
-        }
-
-        Ok::<(), Error>(())
-    })
-    .unwrap();
-}
-
-unsafe fn redirect_to_log_write() {
+unsafe fn start_redirect() {
     let mut logpipe: [RawFd; 2] = Default::default();
     libc::pipe(logpipe.as_mut_ptr());
     libc::dup2(logpipe[1], libc::STDOUT_FILENO);
@@ -82,32 +50,17 @@ unsafe fn redirect_to_log_write() {
                 if len == 0 {
                     break;
                 } else {
-                    try_log_write(buffer.clone());
+                    try_log_write(&buffer);
                 }
             }
         }
     });
 }
 
-unsafe fn redirect_to_log_app(env: &mut JNIEnv) {
-    let exec: Executor;
-    let io_globalref: GlobalRef;
-    let io_log_info_jmethodid: JMethodID;
-    {
-        let jvm = env.get_java_vm().unwrap();
-        exec = Executor::new(Arc::new(jvm));
-        let io_jclass = env.find_class("com/atstudio/denort/jni/Io").unwrap();
-        let io_jobject = env
-            .get_static_field(&io_jclass, "INSTANCE", "Lcom/atstudio/denort/jni/Io;")
-            .unwrap()
-            .l()
-            .unwrap();
-        io_globalref = env.new_global_ref(io_jobject).unwrap();
-        io_log_info_jmethodid = env
-            .get_method_id(&io_jclass, "logInfo", "(Ljava/lang/String;)V")
-            .unwrap();
-    }
-
+unsafe fn start_redirect_only<F>(f: F)
+where
+    F: Fn(&String) + Send + 'static,
+{
     let mut logpipe: [RawFd; 2] = Default::default();
     libc::pipe(logpipe.as_mut_ptr());
     libc::dup2(logpipe[1], libc::STDOUT_FILENO);
@@ -122,32 +75,17 @@ unsafe fn redirect_to_log_app(env: &mut JNIEnv) {
                 if len == 0 {
                     break;
                 } else {
-                    try_log_app(&exec, &io_globalref, &io_log_info_jmethodid, buffer.clone());
+                    f(&buffer);
                 }
             }
         }
     });
 }
 
-unsafe fn redirect_to_log_write_log_app(env: &mut JNIEnv) {
-    let exec: Executor;
-    let io_globalref: GlobalRef;
-    let io_log_info_jmethodid: JMethodID;
-    {
-        let jvm = env.get_java_vm().unwrap();
-        exec = Executor::new(Arc::new(jvm));
-        let io_jclass = env.find_class("com/atstudio/denort/jni/Io").unwrap();
-        let io_jobject = env
-            .get_static_field(&io_jclass, "INSTANCE", "Lcom/atstudio/denort/jni/Io;")
-            .unwrap()
-            .l()
-            .unwrap();
-        io_globalref = env.new_global_ref(io_jobject).unwrap();
-        io_log_info_jmethodid = env
-            .get_method_id(&io_jclass, "logInfo", "(Ljava/lang/String;)V")
-            .unwrap();
-    }
-
+unsafe fn start_redirect_also<F>(f: F)
+where
+    F: Fn(&String) + Send + 'static,
+{
     let mut logpipe: [RawFd; 2] = Default::default();
     libc::pipe(logpipe.as_mut_ptr());
     libc::dup2(logpipe[1], libc::STDOUT_FILENO);
@@ -162,25 +100,31 @@ unsafe fn redirect_to_log_write_log_app(env: &mut JNIEnv) {
                 if len == 0 {
                     break;
                 } else {
-                    try_log_write(buffer.clone());
-                    try_log_app(&exec, &io_globalref, &io_log_info_jmethodid, buffer.clone());
+                    try_log_write(&buffer);
+                    f(&buffer);
                 }
             }
         }
     });
 }
 
-pub fn init_logger_for_log_write() {
+pub fn init_logger() {
     android_logger::init_once(Config::default().with_min_level(Level::Trace));
-    unsafe { redirect_to_log_write() };
+    unsafe { start_redirect() };
 }
 
-pub fn init_logger_for_log_app(env: &mut JNIEnv) {
+pub fn init_logger_only<F>(f: F)
+where
+    F: Fn(&String) + Send + 'static,
+{
     android_logger::init_once(Config::default().with_min_level(Level::Trace));
-    unsafe { redirect_to_log_app(env) };
+    unsafe { start_redirect_only(f) };
 }
 
-pub fn init_logger_for_log_write_log_app(env: &mut JNIEnv) {
+pub fn init_logger_also<F>(f: F)
+where
+    F: Fn(&String) + Send + 'static,
+{
     android_logger::init_once(Config::default().with_min_level(Level::Trace));
-    unsafe { redirect_to_log_write_log_app(env) };
+    unsafe { start_redirect_also(f) };
 }
